07a270697453419c00873dd0e51fa1b1d3432248
gaguajardo <gaguajardo@sodimac.cl>
2020-04-17 13:49:11 -0400

    if y else

---
+++ Ej-if-else/src/ifelse/IfElse.java
@@ -0,0 +1,19 @@
+package ifelse;
+
+public class IfElse {
+	
+	public static void main(String[] args) {
+		
+		int num = 1;
+		
+		if (num < 10) {
+			
+			System.out.println("El numero es menor a 10");
+		}else {
+			
+			System.out.println("El numero es mayor a 10");
+		}
+		
+	}
+
+}
